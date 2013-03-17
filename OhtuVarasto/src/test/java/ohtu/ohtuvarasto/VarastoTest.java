9d7e9f03413c1139fde40aacf23b693456dae009
Lydia Lairala <lydia.lairala@helsinki.fi>
2013-03-17 23:31:14 +0200

    tehtävä9

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -87,7 +87,7 @@ public class VarastoTest {
     @Test
     public void varastoTyhja() {
         varasto.lisaaVarastoon(5);
-        varasto.otaVarastosta(6);
+        varasto.otaVarastosta(8);
         assertEquals(10, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
